c6343e8d9eaebf0b99389884b84ca58189050e8c
Timo-u.de <timo@timo-u.de>
2020-01-26 19:32:25 +0100

    Bugfix: wenn das Feld "payload_fields" nicht existierte wurde ein Fehler im Log angezeigt. Nun wird der Fehler nur noch im Debug der Instanz angezeigt

--- TtnObjectDevice/module.php
+++ TtnObjectDevice/module.php
@@ -92,9 +92,16 @@ class TtnObjectDevice extends IPSModule
             $elements = json_decode($payload);
             $this->SendDebug('ReceiveData()', 'Payload: '.$payload, 0);
         } else {
-            $elements = $data->payload_fields;
-            $this->SendDebug('ReceiveData()', 'Payload: '.json_encode($elements), 0);
-        }
+			if(array_key_exists('payload_fields', $data))
+			{	
+				$elements = $data->payload_fields;
+				$this->SendDebug('ReceiveData()', 'Payload: '.json_encode($elements), 0);
+			}
+			else
+			{	$elements = null;
+				$this->SendDebug('ReceiveData()', 'Key: payload_fields does not exist', 0);
+			}
+		}
         if ($elements == null) {
             $this->SendDebug('ReceiveData()', 'JSON-Decode failed', 0);
         } else {
