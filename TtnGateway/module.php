326ffc5c62b84429d902c4648b3a7531b2e0c76d
timo-u <timoum@web.de>
2020-02-17 08:33:13 +0000

    Apply fixes from StyleCI

--- TtnGateway/module.php
+++ TtnGateway/module.php
@@ -72,50 +72,44 @@ class TtnGateway extends IPSModule
     public function Update()
     {
         $eui = strtolower($this->ReadPropertyString('GatewayId'));
-        if ($eui == 'eui-ffffffffffffffff') 
-		{
-			$this->SendDebug('Update()', '$eui == eui-ffffffffffffffff (request stopped) ' , 0);
+        if ($eui == 'eui-ffffffffffffffff') {
+            $this->SendDebug('Update()', '$eui == eui-ffffffffffffffff (request stopped) ', 0);
+
             return;
         }
         $url = 'http://noc.thethingsnetwork.org:8085/api/v2/gateways/'.$eui;
 
-        
-			$curl = curl_init();
+        $curl = curl_init();
 
-			curl_setopt_array($curl, array(
-				CURLOPT_URL => $url,
-				CURLOPT_RETURNTRANSFER => true,
-				CURLOPT_ENCODING => "",
-				CURLOPT_MAXREDIRS => 10,
-				CURLOPT_TIMEOUT => 0,
-				CURLOPT_FOLLOWLOCATION => true,
-				CURLOPT_HTTP_VERSION => CURL_HTTP_VERSION_1_1,
-				CURLOPT_CUSTOMREQUEST => "GET",
-			));
+        curl_setopt_array($curl, [
+            CURLOPT_URL            => $url,
+            CURLOPT_RETURNTRANSFER => true,
+            CURLOPT_ENCODING       => '',
+            CURLOPT_MAXREDIRS      => 10,
+            CURLOPT_TIMEOUT        => 0,
+            CURLOPT_FOLLOWLOCATION => true,
+            CURLOPT_HTTP_VERSION   => CURL_HTTP_VERSION_1_1,
+            CURLOPT_CUSTOMREQUEST  => 'GET',
+        ]);
 
-			$content = curl_exec($curl);
-			$statusCode = curl_getinfo($curl, CURLINFO_HTTP_CODE);
-			curl_close($curl);
-			//echo $content;
+        $content = curl_exec($curl);
+        $statusCode = curl_getinfo($curl, CURLINFO_HTTP_CODE);
+        curl_close($curl);
+        //echo $content;
 
-			
-		
-		$this->SendDebug('Update() Content: ', $content, 0);
-		$this->SendDebug('Update() HTTP statusCode: ', $statusCode, 0);
+        $this->SendDebug('Update() Content: ', $content, 0);
+        $this->SendDebug('Update() HTTP statusCode: ', $statusCode, 0);
 
+        if ($statusCode == 404) {
+            $this->SetStatus(201);
+            $this->SendDebug('Update() ', 'Status not found. Invalid Gateway ID?', 0);
 
-		if($statusCode == 404) 
-		{
-			$this->SetStatus(201);
-			$this->SendDebug('Update() ', "Status not found. Invalid Gateway ID?", 0);
-			return;
-		}
+            return;
+        }
 
-		if($statusCode != 200) 
-		{
-			return;
-		}
-		
+        if ($statusCode != 200) {
+            return;
+        }
 
         $data = json_decode((string) $content);
 
@@ -146,6 +140,6 @@ class TtnGateway extends IPSModule
             $this->SetValue('downlink', $data->downlink);
         }
         $this->SetValue('lastseenbevore', $difference);
-		$this->SetStatus(102);
+        $this->SetStatus(102);
     }
 }
