de6cf782522e3750831f2d93cdd40b2754ee8dae
Timo-u.de <timo@timo-u.de>
2020-08-12 13:12:51 +0200

    Fix für Syntax-Fehler im TtnDevice

--- TtnDevice/module.php
+++ TtnDevice/module.php
@@ -88,7 +88,7 @@ declare(strict_types=1);
             $this->SendDebug('ReceiveData()', 'Best RSSI: '.$rssi, 0);
 
             if ($this->ReadPropertyBoolean('ShowMeta')) {
-                if (property_exists($metadata , ('frequency')) {
+                if (property_exists($metadata , 'frequency')) {
                     $this->SetValue('Meta_Informations', 'Freq: '.$metadata->frequency.
                 ' Modulation: '.$metadata->modulation.
                 ' Data Rate: '.$metadata->data_rate.
