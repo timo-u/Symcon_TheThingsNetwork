82ac94927c23441e25a7a4add857e04a4774cf89
timo-u.de <kontakt@timo-u.de>
2023-12-29 22:38:16 +0100

    CUR Timeout bei TTNGateway auf 1s gesetzt da ein Timeout von 0 bei IPS >=7.0 nicht mehr unterstützt wird.

--- TtnGateway/module.php
+++ TtnGateway/module.php
@@ -90,7 +90,7 @@ class TtnGateway extends IPSModule
           CURLOPT_RETURNTRANSFER => true,
           CURLOPT_ENCODING => '',
           CURLOPT_MAXREDIRS => 10,
-          CURLOPT_TIMEOUT => 0,
+          CURLOPT_TIMEOUT => 1,
           CURLOPT_FOLLOWLOCATION => true,
           CURLOPT_HTTP_VERSION => CURL_HTTP_VERSION_1_1,
           CURLOPT_CUSTOMREQUEST => 'GET',
